f85df9faacd1b144ab71c4cad19f813c296ca57e
Joseph Scibetta <joseph.scibetta@ramboll.co.uk>
2020-07-03 11:37:00 +0100

    changed program structure

---
+++ MainScraper/DiscordBot.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+
+
+namespace MainScraper
+{
+    public class DiscordBot
+    {
+        //bot named _client
+        private DiscordSocketClient _client;
+        
+        //start new bot
+        public static void RunBot()
+          => new DiscordBot().StartAsync().GetAwaiter();
+
+        public async Task StartAsync()
+        {
+            _client = new DiscordSocketClient();
+
+            await _client.LoginAsync(TokenType.Bot, "discord bot token");
+
+            await _client.StartAsync();
+
+            //send the message. currently doesn't run when required
+            _client.Ready += () =>
+            ((ISocketMessageChannel)_client.GetChannel(728287924495319120)).SendMessageAsync("New Goal: " + Program.newPostTitle + " | " + " URL: " + Program.newPostURL);
+
+            //continue running until closed
+            await Task.Delay(-1);
+
+
+
+        }
+
+
+
+
+
+
+    }
+}
--- MainScraper/Program.cs
+++ MainScraper/Program.cs
@@ -1,15 +1,108 @@
-﻿using System;
+﻿using Reddit;
+using Reddit.Controllers;
+using Reddit.Controllers.EventArgs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using Discord;
+using Discord.WebSocket;
 using System.Threading.Tasks;
 
 namespace MainScraper
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+    //global vars to be passed to discord bot
+    public static string stringToCheck { get; set; }
+    public static string newPostURL { get; set; }
+    public static string newPostTitle { get; set; }
+
+    //init new reddit client and begin monitoring subreddit
+    static void Main(string[] args)
+    {
+        if (args.Length < 2)
         {
+            Console.WriteLine("Usage: Example <Reddit App ID> <Reddit Refresh Token> [Reddit Access Token]");
+        }
+        else
+        {
+            string appId = args[0];
+            string refreshToken = args[1];
+            string appSecret = (args.Length > 2 ? args[2] : null);
+
+            RedditClient reddit = new RedditClient(appId: appId, refreshToken: refreshToken, appSecret: appSecret);
+
+            Subreddit sub = reddit.Subreddit("soccer").About();
+
+            Console.WriteLine("Subreddit Name: " + sub.Name);
+            Console.WriteLine("Subreddit Fullname: " + sub.Fullname);
+            Console.WriteLine("Subreddit Title: " + sub.Title);
+            Console.WriteLine("Subreddit Description: " + sub.Description);
+
+            List<Post> newPosts = sub.Posts.New;
+
+            Console.WriteLine("Retrieved " + newPosts.Count.ToString() + " new posts.");
+
+            Console.WriteLine("Monitoring " + sub.Name + " for new posts....");
+
+            sub.Posts.NewUpdated += C_NewPostsUpdated;
+            sub.Posts.MonitorNew();
+
+        }
+        DiscordBot.RunBot();
+    }
+
+
+
+    // Event handler for handling monitored new posts as they come in. Filters down to relevant posts. 
+    public static void C_NewPostsUpdated(object sender, PostsUpdateEventArgs e)
+    {
+        foreach (Post post in e.Added)
+        {
+            string[] teams = {"Leeds",
+                "Arsenal",
+                "Chelsea",
+                "Manchester United",
+                "Manchester City",
+                "Liverpool",
+                "Leicester",
+                "Bournemouth",
+                "Burnley",
+                "Brighton",
+                "Norwich City",
+                "Aston Villa",
+                "Everton",
+                "Wolves",
+                "Tottenham",
+                "Spurs",
+                "Crystal Palace",
+                "West Ham",
+                "Sheffield United",
+                "Watford",
+                "Southampton",
+                "Newcastle"};
+
+            string[] mediaSites = { "streamja.com",
+                    "streamable.com",
+                    "clippituser.tv" };
+
+            //the following block throws multiple exceptions regarding SelfPost / LinkPost, and something to do with objects
+            //LinkPost throws exception when not checked against post.Listing.IsSelf?
+            //rewrote to avoid LinkPost
+            if (!post.Listing.IsSelf)
+            {
+                stringToCheck = post.Title + post.Listing.URL;
+            }
+
+            if (teams.Any(x => stringToCheck.Contains(x)) && mediaSites.Any(x => stringToCheck.Contains(x)))
+            {
+                newPostTitle = post.Title;
+                newPostURL = post.Listing.URL;
+                Console.WriteLine("TITLE: " + newPostTitle);
+                Console.WriteLine("URL: " + newPostURL);
+
+            }
         }
     }
+}
 }
